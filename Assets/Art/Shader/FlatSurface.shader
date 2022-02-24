8ff1f99bda394226c6a5d8f744a3694ffd6ef68f
gcwhitfield <gwhitfieldjr@gmail.com>
2022-02-24 16:06:48 -0500

    Make scene darker in Prototype

--- Assets/Art/Shader/FlatSurface.shader
+++ Assets/Art/Shader/FlatSurface.shader
@@ -14,7 +14,7 @@ Shader "Custom/FlatSurface"
 
         CGPROGRAM
         // Physically based Standard lighting model, and enable shadows on all light types
-        #pragma surface surf Standard fullforwardshadows
+        #pragma surface surf Lambert fullforwardshadows vertex:vert
 
         // Use shader model 3.0 target, to get nicer looking lighting
         #pragma target 3.0
@@ -24,11 +24,18 @@ Shader "Custom/FlatSurface"
         struct Input
         {
             float2 uv_MainTex;
+            float vertex_normal;
         };
 
         half _Glossiness;
         half _Metallic;
         fixed4 _Color;
+
+        // transfer normals from vertex shader to fragment shader
+        void vert(inout appdata_full v, out Input o) {
+            UNITY_INITIALIZE_OUTPUT(Input, o);
+            o.vertex_normal = abs(v.normal);
+        }
 
         // Add instancing support for this shader. You need to check 'Enable Instancing' on materials that use the shader.
         // See https://docs.unity3d.com/Manual/GPUInstancing.html for more information about instancing.
@@ -37,15 +44,13 @@ Shader "Custom/FlatSurface"
             // put more per-instance properties here
         UNITY_INSTANCING_BUFFER_END(Props)
 
-        void surf (Input IN, inout SurfaceOutputStandard o)
+        void surf (Input IN, inout SurfaceOutput o)
         {
             // Albedo comes from a texture tinted by color
             fixed4 c = tex2D (_MainTex, IN.uv_MainTex) * _Color;
             o.Albedo = c.rgb;
-            // Metallic and smoothness come from slider variables
-            o.Metallic = _Metallic;
-            o.Smoothness = _Glossiness;
             o.Alpha = c.a;
+            o.Normal = IN.vertex_normal;
         }
         ENDCG
     }
--- Assets/Scripts/Gameplay/TextDisplayManager.cs
+++ Assets/Scripts/Gameplay/TextDisplayManager.cs
@@ -5,5 +5,84 @@ using TMPro;
 
 public class TextDisplayManager : Singleton<TextDisplayManager>
 {
-    
+    [SerializeField] private Animator fatherTextAnimator;
+    [SerializeField] private TMP_Text fatherText;
+    [SerializeField] private Animator daughterTextAnimator;
+    [SerializeField] private TMP_Text daughterText;
+
+    public enum TextType
+    {
+        FATHER, // text only on father's side
+        DAUGHTER, // text only on daughter's side
+        CENTER // text in the center of screen [not yet implemented]
+    };
+
+    public void ShowText(TextType type, string text)
+    {
+        ScrollingTextParams textParams;
+        textParams.type = type;
+        textParams.text = text;
+        StartCoroutine("DisplayScrollingText", textParams);
+    }
+
+    struct ScrollingTextParams
+    {
+        public TextType type;
+        public string text;
+    }
+
+    IEnumerator DisplayScrollingText(ScrollingTextParams textParams)
+    {
+        Animator animator = null;
+        TMP_Text text = null;
+        switch(textParams.type)
+        {
+            case TextType.FATHER:
+                animator = fatherTextAnimator;
+                text = fatherText;
+                break;
+            case TextType.DAUGHTER:
+                animator = daughterTextAnimator;
+                text = daughterText;
+                break;
+            case TextType.CENTER:
+                // not implemented
+                yield break;
+        }
+
+        if (!animator) // log warning if animator not found
+        {
+            Debug.LogWarning("Animator for scrolling text not found. Not playing text animation...");
+            yield break;
+        }
+        if (!text) // log error if text not found
+        {
+            Debug.LogError("'TMP_Text' object for text scrolling not found. Text will not be displayed");
+            yield break;
+        }
+
+        // play the open animation    
+        animator.SetTrigger("Open");
+        
+        // display scrolling text
+        float timeBetweenChars = 0.05f; // in seconds
+        int i = 0;
+        text.text = "";
+        while (i < textParams.text.Length)
+        {
+            text.text = text.text + textParams.text[i];
+            yield return new WaitForSeconds(timeBetweenChars);
+        }
+        yield return null;
+
+        // play close animation, disable the textbox
+        animator.SetTrigger("Close");
+        float timer = 0;
+        while (timer < animator.GetCurrentAnimatorClipInfo(0)[0].clip.length + 0.1f) // wait additional 0.1 sec after finish
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        animator.gameObject.SetActive(false);
+    }
 }
