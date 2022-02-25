f6c83dc9a4a10323a5678f695d3593f1cb4e0935
agibeau <agibeau@andrew.cmu.edu>
2022-02-24 23:39:50 -0500

    father animation 8 directions

--- Assets/Scripts/Gameplay/FatherController.cs
+++ Assets/Scripts/Gameplay/FatherController.cs
@@ -17,6 +17,18 @@ public class FatherController : PlayerController
         else Destroy(gameObject);
     }
 
+    private void ResetAnimatorDirections()
+    {
+        animator.SetBool("RunNW", false);
+        animator.SetBool("RunW", false);
+        animator.SetBool("RunSW", false);
+        animator.SetBool("RunS", false);
+        animator.SetBool("RunN", false);
+        animator.SetBool("RunE", false);
+        animator.SetBool("RunNE", false);
+        animator.SetBool("RunSE", false);
+    }
+
     public new void Update()
     {
         base.Update();
@@ -27,6 +39,78 @@ public class FatherController : PlayerController
         AudioManager.Instance.LanternWalkingAudio(gameObject, movement);
 
         Debug.DrawRay(gameObject.transform.position, lookDirection * 10.0f, Color.white, 1.0f);
+
+        Vector2 movement2 = new Vector2(movement.x, movement.z).normalized; //getting movement vector from playercontroller.cs
+        float newhor = movement2[0]; //horizontal portion of unit vector
+        float newver = movement2[1];
+
+        if (newhor == 0 && newver == 0) //character is idle
+        {
+            animator.SetFloat("Speed", 0);
+            //animator.SetBool("TurnEast", true);
+        }
+        else
+        {
+            //Vector2 movement2 = movement1.normalized; //making it into a unit vector
+            /*Vector2 movement2 = new Vector2(movement.x, movement.z).normalized; //getting movement vector from playercontroller.cs
+            float newhor = movement2[0]; //horizontal portion of unit vector
+            float newver = movement2[1]; //vertical portion of unit vector */
+            float angle = Mathf.Rad2Deg * Mathf.Atan2(newver, newhor); //create angle using unit vector and make it into degrees
+            if (angle < 0)
+            {
+                angle = 360 + angle;
+            }
+            //Debug.Log(angle);
+            if (angle <= 10 || angle >= 350) // go east, 20 degree angle
+            {
+                animator.SetFloat("Speed", 1);
+                ResetAnimatorDirections();
+                animator.SetBool("RunE", true);
+            }
+            else if (angle > 10 && angle < 80) //go northeast, 70 degree angle
+            {
+                animator.SetFloat("Speed", 1);
+                ResetAnimatorDirections();
+                animator.SetBool("RunNE", true);
+            }
+            else if (angle >= 80 && angle <= 100) //go north, 20 degree angle
+            {
+                animator.SetFloat("Speed", 1);
+                ResetAnimatorDirections();
+                animator.SetBool("RunN", true);
+            }
+            else if (angle > 100 && angle < 170) //go northwest, 70 degree angle
+            {
+                animator.SetFloat("Speed", 1);
+                ResetAnimatorDirections();
+                animator.SetBool("RunNW", true);
+            }
+            else if (angle >= 170 && angle <= 190) //go west, 20 degree angle
+            {
+                animator.SetFloat("Speed", 1);
+                ResetAnimatorDirections();
+                animator.SetBool("RunW", true);
+            }
+            else if (angle > 190 && angle < 260) //go southwest, 70 degree angle
+            {
+                animator.SetFloat("Speed", 1);
+                ResetAnimatorDirections();
+                animator.SetBool("RunSW", true);
+            }
+            else if (angle >= 260 && angle <= 280) //go south, 20 degree angle
+            {
+                animator.SetFloat("Speed", 1);
+                ResetAnimatorDirections();
+                animator.SetBool("RunS", true);
+            }
+            else //go southeast, 70 degree angle
+            {
+                animator.SetFloat("Speed", 1);
+                ResetAnimatorDirections();
+                animator.SetBool("RunSE", true);
+            }
+        }
+
     }
 
     public void Shoot()
