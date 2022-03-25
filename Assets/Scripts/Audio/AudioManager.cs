2b17e26be293a81117fecb7a2130b5812d1f9ad2
Joshua Rain <1014054551@qq.com>
2022-03-25 08:53:51 -0400

    monster, wolf chase sound effect

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -18,6 +18,7 @@ public class AudioManager : MonoBehaviour
     public EventReference inputCode;
     public EventReference monsterApproach;
     public EventReference bone;
+    public EventReference wolfChase;
     //public EventReference lanternLaunchAduio;
 
     private float footstepTimer = 0.0f;
@@ -108,5 +109,10 @@ public class AudioManager : MonoBehaviour
     public void BoneAudio(GameObject gb)
     {
         PlayAudio(bone, gb);
+    }
+
+    public void WolfChaseAudio(GameObject gb)
+    {
+        PlayAudio(wolfChase, gb);
     }
 }
