15ac34a576a4fa5205e6df281e2f2a3d0c27366b
gcwhitfield <gwhitfieldjr@gmail.com>
2022-04-28 16:35:39 -0400

    Add sparkle to level 3. Fence lock can only be used after all keys have been collected

--- Assets/Scripts/Gameplay/Level3Controller.cs
+++ Assets/Scripts/Gameplay/Level3Controller.cs
@@ -5,15 +5,69 @@ using UnityEngine;
 public class Level3Controller : Singleton<Level3Controller>
 {
 
+    [Header("Father and daughter screens")]
     public GameObject fatherScreen;
     public GameObject daughterScreen;
     public GameObject dualScreen;
-    public Light daughterLight;
 
+    [Header("Gameplay object references")]
+    public Light daughterLight;
     public Interactable clearingEntranceInteractable;
     public Interactable levelCompletedInteractable;
+    public GameObject keysSparkle;
+    public Interactable key1;
+    public Interactable key2;
+    public Interactable key3;
+    public Interactable key4;
+    public Collider fenceLockCollider; // the collider that is attached to the fence lock.
+    // this collider should initially be DISABLED, which makes the player unable to interact with the
+    // fence lock at first. Then, when the player collects all 4 keys, the fence lock will
+    // get enabled and they can walk up to it to activate the fence lock UI
+    bool key1Collected = false;
+    bool key2Collected = false;
+    bool key3Collected = false;
+    bool key4Collected = false;
 
+    [Header("Scene Management")]
     public string nextLevel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        clearingEntranceInteractable.ExecuteOnTriggerEnter(OnClearingReached);
+        levelCompletedInteractable.ExecuteOnTriggerEnter(OnLevelCompleted);
+
+        // set up interaction events for the key collectables
+        key1.ExecuteOnInteract(Key1Collected);
+        key2.ExecuteOnInteract(Key2Collected);
+        key3.ExecuteOnInteract(Key3Collected);
+        key4.ExecuteOnInteract(Key4Collected);
+    }
+
+    // called when the first key is collected
+    public void Key1Collected()
+    {
+        key1Collected = true;
+    }
+
+    // called when the second key is collected
+    public void Key2Collected()
+    {
+        key2Collected = true;
+    }
+
+    // called when the third key is collected
+    public void Key3Collected()
+    {
+        key3Collected = true;
+    }
+
+    // called when the final key is collected
+    public void Key4Collected()
+    {
+        key4Collected = true;
+    }
 
     // this coroutine is called when the clearing is reached in Level 3
     IEnumerator DoClearingReached()
@@ -46,6 +100,16 @@ public class Level3Controller : Singleton<Level3Controller>
                 t -= Time.deltaTime;
                 yield return null;
             }
+
+            // wait for the player to collect all of the keys
+            while (!key1Collected || !key2Collected || !key3Collected || !key4Collected)
+            {
+                yield return null;
+            }
+
+            // active the fence lock. after the fence lock has been activated, the daughter
+            // can walk up to the fence lock to use it
+            fenceLockCollider.enabled = true;
         }
         yield return null;
     }
@@ -60,12 +124,5 @@ public class Level3Controller : Singleton<Level3Controller>
     public void OnLevelCompleted()
     {
         SceneTransitionManager.Instance.TransitionToScene(nextLevel);
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        clearingEntranceInteractable.ExecuteOnTriggerEnter(OnClearingReached);
-        levelCompletedInteractable.ExecuteOnTriggerEnter(OnLevelCompleted);
     }
 }
