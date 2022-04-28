c45c3fbff07bfdc08a0ecddd5ae56be7fdaab7bd
gcwhitfield <gwhitfieldjr@gmail.com>
2022-04-28 09:45:54 -0400

    Add instructional text to level 1. Update pinecone art

--- Assets/Scripts/Gameplay/Level1Controller.cs
+++ Assets/Scripts/Gameplay/Level1Controller.cs
@@ -11,11 +11,21 @@ public class Level1Controller : Singleton<Level1Controller>
     public TextDisplayOnTriggerEnter daughterHideDisplayCheckpoint;
     public GameObject daughterHideRing;
 
+    // this is the font that is used to display the level instructions dialogue at the
+    // beginning of level 1
+    public TMPro.TMP_FontAsset daughterIntructionsFont;
+    public TMPro.TMP_FontAsset fatherIntructionsFont;
+
     private void Start()
     {
         // activate the daughter's hide ring when the daughter reaches the text display
         // checkpoint
         daughterHideDisplayCheckpoint.ExecuteOnTriggerEnter(ShowHideRing);
+
+        //  show the level intructions text to the players at the start of the level
+        
+        TextDisplayManager.Instance.ShowText("Collect pinecones and explore the area.", TextDisplayManager.TextType.DAUGHTER, daughterIntructionsFont);
+        TextDisplayManager.Instance.ShowText("Find a way to open the fence gate.", TextDisplayManager.TextType.FATHER, fatherIntructionsFont);
     }
 
     public void ShowHideRing()
